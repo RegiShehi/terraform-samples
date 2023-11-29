a85a092ff8ba272e4dbeec8dd5177e9f6ffc4c82
Regi Shehi <regi_shehi@hotmail.com>
2023-11-29 21:27:40 +0100

    Fix bucket policy

--- custom-module/main.tf
+++ custom-module/main.tf
@@ -56,19 +56,21 @@ resource "aws_s3_bucket_acl" "my-static-website" {
 resource "aws_s3_bucket_policy" "bucket-policy" {
   bucket = aws_s3_bucket.my-static-website.id
   policy = <<EOF
-    {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Sid": "PublicReadAccess",
-                "Effect": "Allow",
-                "Principal": "*"
-                "Action": [
-                    "s3:GetObject"
-                ],
-                "Resource": "arn:aws:s3:::${var.bucket_name}/*",
-            }
-        ]
-    }
-    EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Sid": "PublicReadGetObject",
+          "Effect": "Allow",
+          "Principal": "*",
+          "Action": [
+              "s3:GetObject"
+          ],
+          "Resource": [
+              "arn:aws:s3:::${var.bucket_name}/*"
+          ]
+      }
+  ]
+}  
+EOF
 }
