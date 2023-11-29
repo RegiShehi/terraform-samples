6ed639f8b44a92f834cb8c6c8b3417f50c5cb3c7
Regi Shehi <regi_shehi@hotmail.com>
2023-11-29 21:25:02 +0100

    Bug fix

--- custom-module/outputs.tf
+++ custom-module/outputs.tf
@@ -10,12 +10,12 @@ output "name" {
 
 output "domain" {
   description = "Domain Name of S3 Bucket"
-  value       = aws_s3_bucket.my-static-website.website_domain
+  value       = aws_s3_bucket_website_configuration.my-static-website.website_domain
 }
 
 output "endpoint" {
   description = "Endpoint of S3 Bucket"
-  value       = aws_s3_bucket.my-static-website.website_endpoint
+  value       = aws_s3_bucket_website_configuration.my-static-website.website_endpoint
 }
 
 
